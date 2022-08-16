248866f48a8f84d93cf6fec747af10d119cff491
EddieRicobom <81531340+EddieRicobom@users.noreply.github.com>
2022-08-16 11:25:25 -0300

    Update parallax_card.js

--- js/parallax_card.js
+++ js/parallax_card.js
@@ -29,7 +29,7 @@ $(function() {
 
 
     function updateReflection(degree, percentage) {
-        card.style.background = `linear-gradient(${degree}deg, rgba(150,150,150,0) 0%,rgba(150,150,150,0.2) ${percentage}%,rgba(150,150,150,0) 100%), url('${imgUrl}')`
+        card.style.background = `linear-gradient(${degree}deg, rgba(82,82,82,0) 0%,rgba(82,82,82,0.2) ${percentage}%,rgba(82,82,82,0) 100%), url('${imgUrl}')`
         card.style.backgroundSize = "cover"
     }
 
